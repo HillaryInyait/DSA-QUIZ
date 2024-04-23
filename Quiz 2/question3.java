ec7b042e39a7addd2d3482f567653ae41f56a6ad
HillaryInyait <145867294+HillaryInyait@users.noreply.github.com>
2024-04-23 19:28:11 +0300

    Add files via upload

---
+++ Quiz 2/question3.java
@@ -0,0 +1,24 @@
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+//Solution
+public class question3 {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextTemp = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+        return prev;
+    }
+}
